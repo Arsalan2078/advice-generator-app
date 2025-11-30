637b92a908425347b7e8339d13edf1c4cf7a259a
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-29 19:10:27 -0500

    App stylized

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import css from "./App.module.scss";
 
 export default function App() {
-    return <div className={css.app}>App</div>;
+    return (
+        <main className={css.app}>
+            <h1>Advice Generator App</h1>
+
+            <div>App</div>
+        </main>
+    );
 }
