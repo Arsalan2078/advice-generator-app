954a5cfc342fe7d224f296f8603c8c5f1578fb14
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-29 19:30:06 -0500

    card stylized

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import css from "./App.module.scss";
+import AdviceGenerator from "./components/AdviceGenerator";
 
 export default function App() {
     return (
         <main className={css.app}>
             <h1>Advice Generator App</h1>
 
-            <div>App</div>
+            <AdviceGenerator />
         </main>
     );
 }
---
+++ src/components/AdviceGenerator/index.tsx
@@ -0,0 +1,9 @@
+import css from "./index.module.scss";
+
+export default function AdviceGenerator() {
+    return (
+        <div className={css.container}>
+            <div className={css.card}>AdviceGenerator</div>
+        </div>
+    );
+}
